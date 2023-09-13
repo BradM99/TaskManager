600b75421b0b3795529f62cdb778732d056174ba
bradley <55547314+BradM99@users.noreply.github.com>
2023-09-13 22:40:53 +0100

    Update main.cpp
    Comments clarifying the state of main function

--- main.cpp
+++ main.cpp
@@ -7,10 +7,13 @@
 int main() {
     TaskManager taskManager;
 
+    //#002 main needs to be reworked into a while loop to keep the program active until manually closed
+
+    //example tasks to test methods for now
     taskManager.addTask("Skate", "Skate at least 4 times a week", "Weekly", false);
     taskManager.addTask("Update CV", "Update CV with new technologies", "Monthly", false);
     taskManager.addTask("Buy Food", "Buy groceries for the week", "Weekly", false);
-    taskManager.addTask("Study", "Learn CPP and OOP", "25/-9/2023", false);
+    taskManager.addTask("Study", "Learn CPP and OOP", "25/09/2023", false);
 
     taskManager.removeTask();
 
