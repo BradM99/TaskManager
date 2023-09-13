11c625bcc259a63510138d3bc0de868f773591ec
bradley <55547314+BradM99@users.noreply.github.com>
2023-09-14 00:01:40 +0100

    Update TaskManager.h
    Removed unneeded flag member

--- TaskManager.h
+++ TaskManager.h
@@ -8,12 +8,11 @@ class TaskManager {
 private:
     //Array of tasks
     std::vector<Task> tasks;
-    // Flag to indicate if viewTasks was called by removeTask
-    bool calledByRemoveTask;
 
 public:
     //Methods for task class
-    TaskManager() : calledByRemoveTask(false) {}
+    TaskManager() {};
+    void addTask();
     void addTask(const std::string& title, const std::string& description, const std::string& dueDate, bool isComplete);
     void TaskManager::removeTask();
     void viewTasks();
