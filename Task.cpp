5e0bd099ea0a1655a084868cfd7d511097e8e92b
tomlinc98 <tomlinc98@gmail.com>
2023-09-14 18:56:46 +0100

    Integrated the `date` library to manage due date

--- Task.cpp
+++ Task.cpp
@@ -3,51 +3,53 @@
 #include <vector>
 #include "Task.h"
 
+// Default constructor
+Task::Task() : 
+    Title(""), 
+    Description(""),
+    DueDate(date::year{1970}/date::January/1),  // Set a default date using the date library
+    isComplete(false)
+{}
 
-Task::Task() {
-    Title = "";
-    Description = "";
-    DueDate = "";
-    isComplete = false;
-}
-
-
-Task::Task(const std::string& title, const std::string& description, const std::string& dueDate, bool isComplete) {
-    Title = title;
-    Description = description;
-    DueDate = dueDate;
-    this->isComplete = isComplete;
-}
-
+// Parameterized constructor
+Task::Task(const std::string& title, 
+           const std::string& description, 
+           const date::year_month_day& dueDate, 
+           bool isComplete) : 
+    Title(title), 
+    Description(description), 
+    DueDate(dueDate), 
+    isComplete(isComplete)
+{}
 
 const std::string& Task::getTitle() const {
     return Title;
 }
 
-void Task::setTitle(const std::string& title){
+void Task::setTitle(const std::string& title) {
     Title = title;
 }
 
-const std::string& Task::getDescription() const{
+const std::string& Task::getDescription() const {
     return Description;
 }
 
-void Task::setDescription(const std::string& description){
+void Task::setDescription(const std::string& description) {
     Description = description;
 }
 
-const std::string& Task::getDueDate() const{
+date::year_month_day Task::getDueDate() const {
     return DueDate;
 }
 
-void Task::setDueDate(const std::string& dueDate){
+void Task::setDueDate(const date::year_month_day& dueDate) {
     DueDate = dueDate;
 }
 
-bool Task::getIsComplete() const{
+bool Task::getIsComplete() const {
     return isComplete;
 }
 
-void Task::setIsComplete(bool newIsComplete){
+void Task::setIsComplete(bool newIsComplete) {
     isComplete = newIsComplete;
 }
